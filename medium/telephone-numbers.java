5ffc0b715b1f135558f10f2c94d597b01ce34b27
= <lucas.marques@etu.u-bordeaux.fr>
2019-11-14 17:52:10 +0100

    Ajout numéro téléphones => arbres et dénombrement

--- medium/telephone-numbers.java
+++ medium/telephone-numbers.java
@@ -2,20 +2,76 @@ import java.util.*;
 import java.io.*;
 import java.math.*;
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
+
+class Tree {
+	List<Tree> childrens;
+	char value;
+	
+	Tree() {
+		childrens = new ArrayList<>();   
+	}
+	
+	Tree(char value) {
+		childrens = new ArrayList<>();
+		
+		this.value = value;
+	}
+	
+	Tree adopt(Tree child) {
+		if(!childrens.stream().anyMatch(e -> (e.value == child.value))) {
+			childrens.add(child);      
+		}            
+		Optional<Tree> node = childrens.stream().filter(e -> {
+			return e.value == child.value; 
+		}).findFirst();
+		if(!node.isPresent())
+			throw new RuntimeException("whoops cpu fucked up");
+		return node.get();
+
+	}
+	
+	boolean is_leaf() {
+		return (childrens.size() == 0);
+	}
+}
 class Solution {
 
-    public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
-        
-        int N = in.nextInt();
-        for (int i = 0; i < N; i++) {
-            String telephone = in.next();
-        }
+	static int count_nodes(Tree node) {
+		int total = 0;
+		if(node.is_leaf()) {
+			return 0;   
+		} else {
+			for(Tree child : node.childrens) {
+				total += count_nodes(child) + 1;
+			}
+		}
+		return total;
+	}
 
-        
-    }
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
+		List<String> telephones = new ArrayList<String>();
+		int N = in.nextInt();
+		for (int i = 0; i < N; i++) {
+			telephones.add(in.next());
+		}
+		Collections.sort(telephones);
+		
+		Tree root = new Tree();
+		
+		for(String telephone : telephones) {
+			char[] telephone_chars = telephone.toCharArray();
+			Tree last_node = root;
+			for(int i = 0; i < telephone_chars.length; i++) {
+				last_node = last_node.adopt(new Tree(telephone_chars[i]));
+			}
+		}
+		
+		int nb_nodes = count_nodes(root);
+		
+		
+		
+		System.out.println(nb_nodes);
+		
+	}
 }
