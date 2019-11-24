3771d9d11b92d22d8c5ac9b719083864019bdfdd
Lucas Marques <6555778+IkeYeek@users.noreply.github.com>
2019-11-24 14:22:20 +0100

    Create isbn-check-digit.py

---
+++ isbn-check-digit.py
@@ -0,0 +1,51 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+isbns = []
+for i in range(n):
+    isbns.append(input())
+
+invalids = []
+
+for isbn in isbns:
+    valid = True
+    check = -1
+    if len(isbn) == 10:
+        summed = 0
+        for i in range(9):
+            if not isbn[i].isnumeric():
+                valid = False
+            else:
+                k = int(isbn[i])
+                summed += (10 - i) * k
+                remainder = summed % 11
+                check = (11 - remainder) 
+                if isbn == '0387372350':
+                    print(check, file=sys.stderr)
+                if check == 10:
+                    check = 'X'
+                elif check == 11:
+                    check = 0
+    elif len(isbn) == 13:
+        summed = 0
+        for i in range(12):
+            if not isbn[i].isnumeric():
+                valid = False
+            else:
+                k = int(isbn[i])
+                summed += k if i%2 == 0 else 3*k
+                remainder = summed % 10
+                check = (10 - remainder) 
+                if check == 10:
+                    check = 0
+            
+    if str(check) != str(isbn[-1]) or not valid:
+        invalids.append(isbn)
+
+print("%d invalid:" % (len(invalids), ))
+for invalid in invalids:
+    print(invalid)
